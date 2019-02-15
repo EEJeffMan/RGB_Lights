22b34afcb3212dd7d436a958761f771292f5ccf3
EEJeffMan <EE.JeffMan@gmail.com>
2019-02-15 07:29:47 -0600

    Updated code with set_leds() function for individual on/off control, and updated comments.

--- Firmware/RGB_Lights/RGB_Lights/RGB_Lights.ino
+++ Firmware/RGB_Lights/RGB_Lights/RGB_Lights.ino
@@ -11,7 +11,7 @@
  *    RGB's use "FastLED" library, credit to tutorial in link below:
  *    https://howtomechatronics.com/tutorials/arduino/how-to-control-ws2812b-individually-addressable-leds-using-arduino/
  *     
- *    6 LEDs:
+ *    6 LEDs, indexed in this order:
  *      Purple
  *      Blue
  *      Red
@@ -63,14 +63,20 @@
  #define ORANGE_B  0   
 
  // LED index (aka order of colors)
- #define PURPLE_LED   0
- #define BLUE_LED     1
- #define RED_LED      2
- #define ORANGE_LED   3
- #define GREEN_LED    4
- #define YELLOW_LED   5
+ #define PURPLE_LED   0b00000001
+ #define BLUE_LED     0b00000010
+ #define RED_LED      0b00000100
+ #define ORANGE_LED   0b00001000
+ #define GREEN_LED    0b00010000
+ #define YELLOW_LED   0b00100000
+
+ void set_leds(unsigned int led_configuration);
 
 CRGB leds[NUM_LEDS];
+
+const unsigned int red[6] = {PURPLE_R,BLUE_R,RED_R,ORANGE_R,GREEN_R,YELLOW_R};
+const unsigned int green[6] = {PURPLE_G,BLUE_G,RED_G,ORANGE_G,GREEN_G,YELLOW_G};
+const unsigned int blue[6] = {PURPLE_B,BLUE_B,RED_B,ORANGE_B,GREEN_B,YELLOW_B};
 
 void setup() {
 
@@ -79,17 +85,42 @@ void setup() {
   // put your setup code here, to run once:
   FastLED.addLeds<WS2812, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 
-  leds[PRUPLE_LED] = CRGB(PURPLE_R, PURPLE_G, PURPLE_B);
-  leds[BLUE_LED] = CRGB(BLUE_R, BLUE_G, BLUE_B);
-  leds[RED_LED] = CRGB(RED_R, RED_G, RED_B);
-  leds[ORANGE_LED] = CRGB(ORANGE_R, ORANGE_G, ORANGE_B);
-  leds[GREEN_LED] = CRGB(GREEN_R, GREEN_G, GREEN_B);
-  leds[YELLOW_LED] = CRGB(YELLOW_R, YELLOW_G, YELLOW_B);
+  set_leds(PURPLE_LED | BLUE_LED | RED_LED | ORANGE_LED | GREEN_LED | YELLOW_LED);
 
   FastLED.show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   
+
+  // TODO: MK II... read analog inputs and use set_leds() function to set outputs accordingly.
 }
+
+void set_leds(unsigned int led_configuration)
+{
+  /* loop through bitmask to check if each LED should be on.
+   * if it should be on, set the appropriate RGB values. 
+   * if it shouldnt be on, set the RGB values to 0.
+   * 
+   * bitmask order follows that of the LED RGB const arrays.
+   */
+
+  unsigned int i;
+  unsigned int temp_mask;
+
+  temp_mask = PURPLE_LED;
+  for (i = 0; i<=5; i++)
+  {
+    if (led_configuration & temp_mask)
+    {
+      leds[i] = CRGB(red[i], green[i], blue[i]);
+    }
+    else
+    {
+      leds[i] = CRGB(0,0,0);
+    }
+    temp_mask = temp_mask << 1;   //next bitmask.
+  }
+}
+
+//end of file.
